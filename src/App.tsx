73bf51aba89a8836ca8dd7688550e21a0e8c779d
Yousfi Abderrahmane <bdrrhmnyousfi@gmail.com>
2023-02-02 07:40:02 +0100

    Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
-import React from "react";
-import "./App.css";
+import { Routes, Route } from "react-router";
+import { Container } from "react-bootstrap";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Store } from "./pages/Store";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container className="mb-4">
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/store"} element={<Store />} />
+        <Route path={"/about"} element={<About />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default App;
